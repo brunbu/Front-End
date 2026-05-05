5f0f3471ff6f6c15bac59c93186576b1e7666f11
BRUNO RODRIGUES QUIRINO DA SILVA <42639848895@info.local>
2026-05-05 16:45:43 -0300

    React

--- ReactJS/simple-page/src/App.jsx
+++ ReactJS/simple-page/src/App.jsx
@@ -3,14 +3,14 @@ import './App.css';
 function App() {
   return(
     <nav class="menu">
-        <a href="#" class="menu__item">Home</a>
-        <a href="#" class="menu__item">Quem Somos</a>
-        <a href="#" class="menu__item">Contato</a>
-        <a href="#" class="menu__item--success">Entrar</a>
-        <a href="#" class="menu__item--button-default">Cadastrar</a>
+        <a href="#" className="menu__item">Home</a>
+        <a href="#" className="menu__item">Quem Somos</a>
+        <a href="#" className="menu__item">Contato</a>
+        <a href="#" className="menu__item--success">Entrar</a>
+        <a href="#" className="menu__item--button-default">Cadastrar</a>
 
-         <div class="card-perfil">
-          <img class="card-perfil__image" src="/images/Doakes Mater.jpg" alt="Imagem de perfil do usuário"></img>
+        <div class="card-perfil">
+        <img class="card-perfil__image" src="/images/Doakes Mater.jpg" alt="Imagem de perfil do usuário"></img>
     </div>
     </nav>
 
