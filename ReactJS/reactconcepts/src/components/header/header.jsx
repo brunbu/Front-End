a9474aaf8b548aa6a5bf787ba10a902e2cc120b0
BRUNO RODRIGUES QUIRINO DA SILVA <42639848895@info.local>
2026-05-14 16:45:16 -0300

    Json-Server e css atualizado

--- ReactJS/reactconcepts/src/components/header/header.jsx
+++ ReactJS/reactconcepts/src/components/header/header.jsx
@@ -6,7 +6,8 @@ export default function Header() {
             <nav>
                 <Link to="/">Home</Link> {" | "}
                 <Link to="/quemsomos">Quem Somos</Link> {" | "}
-                <Link to="/cadfrutas">Frutas</Link>
+                <Link to="/cadfrutas">Frutas</Link> {" | "}
+                <Link to="/produtos">Produtos</Link>
             </nav>
         </header>
     )
